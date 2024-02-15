90f6255ac14473730c15b9e6227c8469e791da2f
Simon Le Marchant <simon@marchantweb.com>
2024-02-15 13:35:14 -0500

    feat: handwritten filter added to primitive demos

--- docs/demo/components/renderer/PrimitiveSVGRenderer.vue
+++ docs/demo/components/renderer/PrimitiveSVGRenderer.vue
@@ -21,7 +21,34 @@ const props = withDefaults(defineProps<Props>(), {
       <text x="15" y="25" fill="white" font-family="JetBrains Mono" font-size="11px" style="text-transform: uppercase; font-weight: 600">{{ label}}</text>
 
       <!-- Render each primitive's SVG path -->
-      <path v-for="primitive in primitives" :d="primitive.svgPath.value" fill="none" stroke="rgba(255, 255, 255, 1)" stroke-width="4"/>
+      <path v-for="primitive in primitives" :d="primitive.svgPath.value" fill="none" stroke="rgba(255, 255, 255, 1)" stroke-width="4" filter="url(#handwrittenFilter)"/>
+
+      <defs>
+        <filter x="-20%" y="-20%" width="140%" height="140%" filterUnits="objectBoundingBox" id="handwrittenFilter">
+          <feTurbulence type="fractalNoise" baseFrequency="2" numOctaves="5" stitchTiles="stitch" result="t1">
+          </feTurbulence>
+          <feColorMatrix type="matrix" values="0 0 0 0 0, 0 0 0 0 0, 0 0 0 0 0, 0 0 0 -1.5 1.5" result="t2">
+          </feColorMatrix>
+          <feComposite operator="in" in2="t2" in="SourceGraphic" result="SourceTextured">
+          </feComposite>
+          <feTurbulence type="fractalNoise" baseFrequency="0.03" numOctaves="5" seed="1" result="f1">
+          </feTurbulence>
+          <feDisplacementMap xChannelSelector="R" yChannelSelector="G" scale="5" in="SourceTextured" in2="f1" result="f4">
+          </feDisplacementMap>
+          <feTurbulence type="fractalNoise" baseFrequency="0.03" numOctaves="5" seed="10" result="f2">
+          </feTurbulence>
+          <feDisplacementMap xChannelSelector="R" yChannelSelector="G" scale="5" in="SourceTextured" in2="f2" result="f5">
+          </feDisplacementMap>
+          <feTurbulence type="fractalNoise" baseFrequency="0.03" numOctaves="5" seed="100" result="f3">
+          </feTurbulence>
+          <feDisplacementMap xChannelSelector="R" yChannelSelector="G" scale="3" in="SourceTextured" in2="f3" result="f6">
+          </feDisplacementMap>
+          <feBlend mode="multiply" in2="f4" in="f5" result="out1">
+          </feBlend>
+          <feBlend mode="multiply" in="out1" in2="f6" result="out2">
+          </feBlend>
+        </filter>
+      </defs>
 
     </svg>
   </div>
